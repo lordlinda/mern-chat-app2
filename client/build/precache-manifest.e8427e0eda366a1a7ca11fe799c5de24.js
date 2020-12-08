687131a764fd9767629b60cfaae325baef8b09d6
lord <marialindananono@gmail.com>
2020-12-08 16:47:17 +0300

    build

---
+++ client/build/precache-manifest.e8427e0eda366a1a7ca11fe799c5de24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb98b25aaf73095d5b586d183ccc657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dfb6ddddd9e835c6ef4",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "ea9db68178355fec6616",
+    "url": "/static/css/main.d0bf1025.chunk.css"
+  },
+  {
+    "revision": "6dfb6ddddd9e835c6ef4",
+    "url": "/static/js/2.f6966fc2.chunk.js"
+  },
+  {
+    "revision": "ea9db68178355fec6616",
+    "url": "/static/js/main.5076a38d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
