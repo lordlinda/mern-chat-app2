f8e091dc4a6cb166c76632b323bcd14945541247
lord <marialindananono@gmail.com>
2020-12-08 16:42:40 +0300

    build

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const config = require("./config/key");
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+const connect = mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,19 +21,18 @@ app.use(cookieParser());
 const { Chat } = require("./models/Chat");
 const { auth } = require("./middleware/auth");
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/chat', require('./routes/chat'));
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/chat", require("./routes/chat"));
 
 const multer = require("multer");
 const fs = require("fs");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}_${file.originalname}`)
+    cb(null, `${Date.now()}_${file.originalname}`);
   },
   // fileFilter: (req, file, cb) => {
   //   const ext = path.extname(file.originalname)
@@ -40,54 +41,52 @@ var storage = multer.diskStorage({
   //   }
   //   cb(null, true)
   // }
-})
- 
-var upload = multer({ storage: storage }).single("file")
-
-app.post("/api/chat/uploadfiles", auth ,(req, res) => {
-  upload(req, res, err => {
-    if(err) {
-      return res.json({ success: false, err })
-    }
-    return res.json({ success: true, url: res.req.file.path });
-  })
 });
 
-io.on("connection", socket => {
+var upload = multer({ storage: storage }).single("file");
 
-  socket.on("Input Chat Message", msg => {
+app.post("/api/chat/uploadfiles", auth, (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({ success: true, url: res.req.file.path });
+  });
+});
 
-    connect.then(db => {
+io.on("connection", (socket) => {
+  socket.on("Input Chat Message", (msg) => {
+    connect.then((db) => {
       try {
-          let chat = new Chat({ message: msg.chatMessage, sender:msg.userId, type: msg.type })
+        let chat = new Chat({
+          message: msg.chatMessage,
+          sender: msg.userId,
+          type: msg.type,
+        });
 
-          chat.save((err, doc) => {
-            console.log(doc)
-            if(err) return res.json({ success: false, err })
+        chat.save((err, doc) => {
+          console.log(doc);
+          if (err) return res.json({ success: false, err });
 
-            Chat.find({ "_id": doc._id })
+          Chat.find({ _id: doc._id })
             .populate("sender")
-            .exec((err, doc)=> {
-
-                return io.emit("Output Chat Message", doc);
-            })
-          })
+            .exec((err, doc) => {
+              return io.emit("Output Chat Message", doc);
+            });
+        });
       } catch (error) {
         console.error(error);
       }
-    })
-   })
-
-})
-
+    });
+  });
+});
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
   // Set static folder
   app.use(express.static("client/build"));
 
@@ -97,8 +96,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+  console.log(`Server Running at ${port}`);
 });
