ac366365b3a0f2492606233a1043f2e78a480d9c
lord <marialindananono@gmail.com>
2020-12-09 12:43:29 +0300

    diff servers

--- server/index.js
+++ server/index.js
@@ -86,7 +86,7 @@ io.on("connection", (socket) => {
 app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+/**if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
@@ -94,7 +94,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
-}
+}**/
 
 const port = process.env.PORT || 5000;
 
